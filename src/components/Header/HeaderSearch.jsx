51b012b6d5661d66a2734a68d52460bf8935d3ac
samratcodes <samratcodess@gmail.com>
2023-06-17 15:21:00 +0545

    Implemented autocomplete on headerSearch comp

--- src/components/Header/HeaderSearch.jsx
+++ src/components/Header/HeaderSearch.jsx
@@ -1,11 +1,52 @@
 import classes from './HeaderSearch.module.scss'
 import { MagnifyingGlass } from '@phosphor-icons/react'
+import { GeoapifyGeocoderAutocomplete } from '@geoapify/react-geocoder-autocomplete'
+import { useMap } from 'react-map-gl'
+import mapboxgl from 'mapbox-gl'
+import useMarkerStore from '../../store/markerStore'
 
 const HeaderSearch = () => {
+  const { map } = useMap()
+
+  const marker = useMarkerStore((state) => state.searchMarker)
+  const setMarker = useMarkerStore((state) => state.setSearchMarker)
+
+  const onSelect = (result) => {
+    if (result) {
+      const { lat, lon: lng } = result.properties
+      console.log(lat, lng)
+
+      if (marker) {
+        marker.remove()
+      }
+
+      const newMarker = new mapboxgl.Marker({
+        color: '#333333b9',
+      })
+        .setLngLat([lng, lat])
+        .addTo(map.getMap())
+
+      setMarker(newMarker)
+
+      map.flyTo({
+        center: [lng, lat],
+        zoom: 12,
+        curve: 2,
+        speed: 1.2,
+      })
+    } else {
+      console.error('Invalid result object:', result)
+    }
+  }
+
   return (
     <form className={classes.header__search}>
       <MagnifyingGlass color="#333333b9" size={24} />
-      <input placeholder="Search for a location" type="text" />
+      <GeoapifyGeocoderAutocomplete
+        filterByCountryCode={['np']}
+        placeholder="Search for a location"
+        placeSelect={onSelect}
+      />
     </form>
   )
 }
