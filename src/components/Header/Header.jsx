76944701fe1e7833c367563f24ba4d8a64a1a4a5
Sls0n <silson0072@gmail.com>
2023-06-17 13:14:28 +0545

    Added phosphor icon and HeaderLogo comp

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import classes from './Header.module.scss'
+import HeaderLogo from './HeaderLogo'
 
 const Header = () => {
-  return <header className={classes.header}>Header</header>
+  return (
+    <header className={classes.header}>
+      <HeaderLogo />
+    </header>
+  )
 }
 
 export default Header
---
+++ src/components/Header/HeaderLogo.jsx
@@ -0,0 +1,24 @@
+import classes from './HeaderLogo.module.scss'
+import { Path } from '@phosphor-icons/react'
+
+const HeaderLogo = () => {
+  return (
+    <div className={classes.header__logo}>
+      <div className={classes['header__logo-text']}>
+        Trip<span>Optima</span>
+        <Path
+          style={{
+            display: 'inline-block',
+            verticalAlign: 'middle',
+            cursor: 'pointer',
+          }}
+          color="#131e24"
+          size={42}
+          weight="duotone"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default HeaderLogo
